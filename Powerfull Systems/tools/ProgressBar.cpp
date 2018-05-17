af2bccdd33bdc27e43c56ea6e09709f867c66232
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2018-05-17 17:13:21 -0300

    Add files via upload

--- Powerfull Systems/tools/ProgressBar.cpp
+++ Powerfull Systems/tools/ProgressBar.cpp
@@ -1,42 +1,62 @@
 #include "ProgressBar.h"
 
-ProgressBar::ProgressBar(uint8_t ui8_maxBar, bool b_printShadow){
+ProgressBar::ProgressBar(uint32_t ui32_maxBar, uint8_t ui32_maxValue){
   this->c_shadowBar = 177;
   this->c_progressBar = 219;
   this->c_frame = 179;
-  this->ui8_maxBar = ui8_maxBar;
-  if(b_printShadow == true){
-    printf("%c", this->c_frame);
-    for(uint8_t ui8_counter = 0; ui8_counter < this->ui8_maxBar; ui8_counter++){
-      printf("%c", this->c_shadowBar);
-    }
-    printf("%c", this->c_frame);
-  }
-  this->ui8_barValue = 0;
+  this->f_percentageValue = 0;
+  this->ui32_maxBar = ui32_maxBar;
+  this->f_maxConstant = (float)ui32_maxBar/(float)ui32_maxValue;
+  this->ui32_barValue = 0;
 }
 
 ProgressBar::~ProgressBar(){
 }
 
-void ProgressBar::showProgressBar(uint8_t ui8_increase){
+void ProgressBar::showProgressBar(uint32_t ui32_increase){
+  showConsoleCursor(false);
+  uint32_t ui32_newIncrease = ui32_roundValue((float)ui32_increase*(float)this->f_maxConstant);
   bool b_flagPrintPercentageProgress = false;
-  if (ui8_increase > ui8_barValue){
+  if ((float)ui32_increase*(float)this->f_maxConstant > ui32_barValue){
     b_flagPrintPercentageProgress = true;
   }
-  if(this->ui8_maxBar >= ui8_increase){
+  if(this->ui32_maxBar >= ui32_newIncrease){
     printf ("\r");
     printf("%c", this->c_frame);
-    for(uint8_t ui8_counter = 0; ui8_counter < ui8_increase; ui8_counter++){
+    for(uint32_t ui32_counter = 0; ui32_counter < ui32_newIncrease; ui32_counter++){
       printf("%c", this->c_progressBar);
     }
     if (b_flagPrintPercentageProgress == true){
-      this->ui8_barValue = ui8_increase;
+      this->ui32_barValue = ui32_newIncrease;
+      this->f_percentageValue = (float)ui32_increase*(float)this->f_maxConstant/(float)this->ui32_maxBar*(float)100;
     }
-    while(ui8_increase < this->ui8_maxBar){
+    while(ui32_newIncrease < this->ui32_maxBar){
       printf("%c", this->c_shadowBar);
-      ui8_increase++;
+      ui32_newIncrease++;
+    }
+    if (f_percentageValue == 100){
+      showConsoleCursor(true);
     }
     printf("%c", this->c_frame);
-    printf(" - %.2f%c", (float)this->ui8_barValue/(float)this->ui8_maxBar*100, 37);
+    printf(" - %.2f%c", (float)this->f_percentageValue, 37);
   }
+}
+
+uint32_t ProgressBar::ui32_roundValue(float f_value){
+  uint32_t ui32_compareValue = (uint32_t)f_value;
+  float f_decimalValue = (float)f_value - (float)ui32_compareValue;
+  if (f_decimalValue >= 0.5){
+    return ui32_compareValue + 1;
+  }
+  else{
+    return ui32_compareValue;
+  }
+}
+
+void ProgressBar::showConsoleCursor(bool b_showFlag){
+  HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_CURSOR_INFO CursorInfo;
+  GetConsoleCursorInfo(out, &CursorInfo);
+  CursorInfo.bVisible = b_showFlag;
+  SetConsoleCursorInfo(out, &CursorInfo);
 }
--- Powerfull Systems/tools/ProgressBar.h
+++ Powerfull Systems/tools/ProgressBar.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <inttypes.h>
+#include <windows.h>
 
 //!  Progress Bar Class
 /*!
@@ -14,27 +15,45 @@ class ProgressBar{
     /*!
       In this private 8-bit integer variable the bar value is stored.
     */
-    uint8_t ui8_barValue;
+    uint32_t ui32_barValue;
     //! Max Bar Value
     /*!
-      In this private 8-bit integer variable the max bar value is stored.
+      In this private 32-bit integer variable the max bar value is stored.
     */
-    uint8_t ui8_maxBar;
+    uint32_t ui32_maxBar;
+    float f_percentageValue;
+    //! Max Constant
+    /*!
+      In this private float variable the division value of ui32_maxBar with ui32_maxValue is stored.
+    */
+    float f_maxConstant;
     //! Character of Shadow Bar
     /*!
       In this private char variable the shadow char value is stored.
     */
     char c_shadowBar;
-    //! Character of Shadow Bar
+    //! Character of Progress Bar
     /*!
       In this private char variable the progress bar char value is stored.
     */
     char c_progressBar;
-    //! Character of Shadow Bar
+    //! Character of Frame
     /*!
       In this private char variable the frame char value is stored.
     */
     char c_frame;
+    //! Show ProgressBar
+    /*!
+      This method will round the value.
+      \param f_value an float argument.
+    */
+    uint32_t ui32_roundValue(float f_value);
+    //! Show Console Cursor
+    /*!
+      This method will show the console cursor.
+      \param f_value an float argument.
+    */
+    void showConsoleCursor(bool b_showFlag);
   public:
     //! Neuron Constructor
     /*!
@@ -42,19 +61,18 @@ class ProgressBar{
       \param ui8_maxBar an 8-bit integer (a.k.a. byte) argument.
       \param b_printShadow a boolean variable.
     */
-    ProgressBar(uint8_t ui8_maxBar, bool b_printShadow);
-    //! Neuron Constructor
+    ProgressBar(uint32_t ui32_maxBar, uint8_t ui32_maxValue);
+    //! Neuron Destructor
     /*!
       This destructor will delete this object.
     */
     ~ProgressBar(void);
-    //! Neuron Constructor
+    //! Show ProgressBar
     /*!
-      This method will increase the progress bar.
-      \param ui8_increase an 8-bit integer (a.k.a. byte) argument.
-
+      This method will show the progress bar.
+      \param ui32_increase an 32-bit integer argument.
     */
-    void showProgressBar(uint8_t ui8_increase);
+    void showProgressBar(uint32_t ui32_increase);
 };
 
 #endif
